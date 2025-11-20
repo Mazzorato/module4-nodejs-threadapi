9379885cc750a39775b1d99642132bed436e7c05
Mazzorato ALEXIS <alexis.mazzorato@gmail.com>
2025-11-20 16:39:07 +0100

    AJout des tables

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@ import { loadSequelize } from "./database.mjs";
 import express from "express";
 import cors from "cors";
 import bcrypt from "bcrypt";
+import { DataTypes } from "sequelize";
 
 /**
  * Point d'entrée de l'application
@@ -10,11 +11,67 @@ import bcrypt from "bcrypt";
 async function main() {
     try {
         const sequelize = await loadSequelize();
+        const User = sequelize.models.User;
+        const Post = sequelize.models.Post;
+        const Comment = sequelize.models.Comment;
         const app = express();
 
+        app.use(express.json());
+
+        app.get("/", (req,res)=>{
+
+            res.json({message:"Hello Task API!"});
+        });
+        // GET http://localhost:3000/user
+        app.get("/user/:id",async (req,res)=>{
+            try {
+
+                const id = req.params.id;
+                const user = await User.findByPk(id)
+                
+                const NewUser = await User.create({
+                    
+                    username: "billy",
+                    email: "billy@gmail.com",
+                    password: "azerty123"
+                });
+
+                res.json(user);
+            } catch (error) {
+                console.log(error);
+                res.status(500).json({message: "Error Serveur"});  
+            }
+        });
+
+        app.get("/post/:id",async (req,res)=>{
+            const id = req.params.id;
+            const post = await post.findByPk(id)
+
+            const NewPost = await Post.create({
+                title : "Premier titre",
+                content: "Info du jour"
+            })
+
+            res.json(post);
+        })
+
+        app.get("/comment/:id",async (req,res)=>{
+            const id = req.params.id;
+            const comment = await comment.findByPk(id)
+
+            const NewComment = await Comment.create({
+                content: "Je commente",
+
+            })
+
+            res.json(comment);
+        })
 
 
 
+        
+
+        
 
         app.listen(3000, () => {
             console.log("Serveur démarré sur http://localhost:3000");
--- database.mjs
+++ database.mjs
@@ -1,4 +1,5 @@
 import { Sequelize, DataTypes } from "sequelize";
+import bcrypt from "bcrypt";
 
 /**
  * 
@@ -6,8 +7,78 @@ import { Sequelize, DataTypes } from "sequelize";
  */
 export async function loadSequelize() {
     try {
-        const sequelize = new Sequelize(/* ... */);
-        // ...
+
+        const login = {
+            database: "app-database",
+            username: "root",
+            password: "root"
+        };
+        const sequelize = new Sequelize("app-database", "root", "root", {
+            host: '127.0.0.1',
+            dialect: 'mysql'
+        });
+
+        //Créations des tables via models:
+        const User = sequelize.define("User", {
+            username: DataTypes.STRING,
+            email: DataTypes.STRING,
+            password: {
+                type: DataTypes.STRING,
+                // Hook pour chiffrer le mot de passe avant de le sauvegarder
+                // +
+                set(clearPassword) {
+                    if (clearPassword && !clearPassword.startsWith("$2b$")) {
+                        // alors c'est un password non-hashé
+                        const hashedPassword = bcrypt.hashSync(clearPassword, 10);
+                        this.setDataValue('password', hashedPassword);
+                    } else {
+                        this.setDataValue('password', clearPassword);
+                    }
+                }
+            }
+        });
+
+        const Post = sequelize.define("Post", {
+            title: DataTypes.STRING,
+            content: DataTypes.STRING,
+        });
+
+        const Comment = sequelize.define("Comment", {
+            content: DataTypes.STRING,
+        });
+
+        // 2 Liaisons forme une Association
+        User.hasMany(Post); // --{
+        Post.belongsTo(User); // --||
+
+        Post.hasMany(Comment);
+        Comment.belongsTo(Post);
+
+        User.hasMany(Comment);
+        Comment.belongsTo(User);
+
+        // Création des tables
+
+        await sequelize.sync ({ force: true});
+        console.log("Connexion à la BDD effectuée");
+
+        //Init fixtures data
+        const userTest = await User.create ({
+            username: "Billy",
+            email: "billy@mail.com",
+            password: "1234"
+        });
+        
+
+
+
+
+
+
+
+        await sequelize.sync({ force: true });
+        console.log("Connexion effectuée");
+
         return sequelize;
     } catch (error) {
         console.error(error);
